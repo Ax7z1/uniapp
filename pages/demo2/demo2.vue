77d6464dc5ec61085769f799453c87f223317cb1
Ax7z1 <Ax7z1@foxmail.com>
2024-03-19 23:01:23 +0800

    对view的style使用v-bind:style= 进行绑定

--- pages/demo2/demo2.vue
+++ pages/demo2/demo2.vue
@@ -2,6 +2,10 @@
 	<view>
 		<view v-on:click="onClick">{{title}}</view>
 		<button @click="clickNum">数值：{{num}}</button>
+		
+		<view class="box" :style="{background: bgcolor}" @click="clickBg">
+			{{random}}
+		</view>
 	</view>
 </template>
 
@@ -10,7 +14,9 @@
 		data() {
 			return {
 				title:"demo2测试",
-				num: 1
+				num: 1,
+				bgcolor:"#c00",
+				random: 0
 			};
 		},
 		methods:{
@@ -19,11 +25,23 @@
 			},
 			clickNum(){
 				this.num++;
+			},
+			clickBg(){
+				let color="#" + String(Math.random()).substring(3,6); //随机色 #342
+				console.log(color)
+				this.bgcolor=color
+				
+				this.random = Math.ceil((Math.random() * 100) + 1);
+				//Js中: Math.ceil 向上取整； Math.floor 向下取整 
 			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.box{
+	width: 200rpx;
+	height: 200rpx;
+	background: pink;
+}
 </style>
