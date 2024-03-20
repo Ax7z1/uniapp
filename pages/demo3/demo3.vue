57f24aded8761af5263ea48686a9191394992fc1
Ax7z1 <Ax7z1@foxmail.com>
2024-03-20 11:21:37 +0800

    v-model及小案例

---
+++ pages/demo3/demo3.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<view @click="onClick">标题：{{title}}</view>
+		<input type="text" v-model:value="title" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: "测试v-model"
+			};
+		},
+		methods:{
+			onClick(){
+				this.title = Math.random()
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+input{
+	border: 1px,solid #ccc;
+}
+</style>
