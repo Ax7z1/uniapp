81149cac42de917b01ec16914d580800e4d59570
Ax7z1 <Ax7z1@foxmail.com>
2024-03-20 11:48:11 +0800

    小案例表单提交-双向绑定与表单配合

--- pages/demo3/demo3.vue
+++ pages/demo3/demo3.vue
@@ -2,6 +2,29 @@
 	<view>
 		<view @click="onClick">标题：{{title}}</view>
 		<input type="text" v-model:value="title" />
+		
+		-------小案例---------
+		
+		<view class="out">
+			<view class="row">
+				<input class="border" type="text" placeholder="请输入姓名.." v-model="message.username">
+			</view>
+			
+			<view class="row">
+				<input class="border" type="text" placeholder="请输入联系电话" v-model="message.tel">
+			</view>
+			
+			<view class="row">
+				<textarea class="border" placeholder="请输入留言内容..." v-model="message.content"></textarea>
+			</view>
+			
+			<view class="row">
+				<button type="primary" @click="onSubmit">提交留言</button>
+			</view>			
+		</view>
+		
+		{{message}}
+
 	</view>
 </template>
 
@@ -9,19 +32,38 @@
 	export default {
 		data() {
 			return {
-				title: "测试v-model"
+				title: "测试v-model",
+				message:{
+					username: "",
+					tel:"",
+					content:""
+				}
 			};
 		},
 		methods:{
 			onClick(){
 				this.title = Math.random()
+			},
+			onSubmit(){
+				console.log(this.message)
 			}
 		}
 	}
 </script>
 
 <style lang="scss">
-input{
-	border: 1px,solid #ccc;
+.out{
+	padding:30rpx;
+	.row{
+		margin-bottom: 10rpx;
+	}
+	.border{
+		border: 1px solid #eee;
+		width: 100%;
+		min-height: 80rpx;
+		padding: 0 20px;
+		box-sizing: border-box;
+	}
 }
+
 </style>
