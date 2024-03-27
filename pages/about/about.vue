814bb9fd4754359859049345f1b8e140ea10cbb6
Ax7z1 <Ax7z1@foxmail.com>
2024-03-27 11:00:30 +0800

    props与this.

--- pages/about/about.vue
+++ pages/about/about.vue
@@ -3,6 +3,9 @@
 		<pubTitle title="我们" subtitle="about page"></pubTitle>
 		<navigator url="/pages/index/index">首页</navigator>
 		<navigator url="/pages/list/list">列表页</navigator>
+		----------------------------------------
+		<myevent title="组件间传值" @myenv="onmyenv"></myevent>
+		
 	</view>
 </template>
 
@@ -12,6 +15,11 @@
 			return {
 				
 			};
+		},
+		methods:{
+			onmyenv(e){
+				console.log(e)
+			}
 		}
 	}
 </script>
